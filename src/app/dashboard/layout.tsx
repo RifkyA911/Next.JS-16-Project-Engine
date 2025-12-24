29f4a3aa168dc7229eb118bd7479d839bbdb1f4a
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-12-24 20:17:36 +0700

    cln: α 0.0.44 -> clean unused components and refactor

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,11 +3,10 @@
 // app/dashboard/layout.tsx
 import React from "react";
 import { ReactQueryProvider } from "@/providers/react-query";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { DashboardSidebar } from "./_components/dashboard-sidebar";
 import { DashboardContainer } from "./_components/dashboard-container";
 import { DashboardNavbar } from "./_components/dashboard-navbar";
-import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 
 export default async function DashboardLayout({
     children,
@@ -21,8 +20,6 @@ export default async function DashboardLayout({
                 <DashboardSidebar />
                 <div className="flex h-screen overflow-hidden w-full">
                     <div className="flex flex-col flex-1 min-w-0 ">
-                        <SidebarTrigger />
-                        <AnimatedThemeToggler />
                         <DashboardNavbar />
                         <DashboardContainer>
                             <main className="flex-1 overflow-auto p-4 min-w-0">
