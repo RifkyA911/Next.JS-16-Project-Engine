2a14c39bae7061a99022f45adbca9f3d7e4058ce
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-12-24 20:12:13 +0700

    feat: α 0.0.36 -> logger middleware

---
+++ src/middlewares/logger.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse, NextFetchEvent } from "next/server";
+
+export async function logMiddleware(req: NextRequest, event: NextFetchEvent) {
+  // const ip = req.headers.get("x-forwarded-for") || "::1";
+  const ip =
+    req.headers.get("x-forwarded-for")?.split(",")[0]?.trim() || "unknown";
+
+  // console.log("req.headers", req);
+
+  // Skip internal logger & asset
+  if (
+    req.url.includes("/api/logger") ||
+    req.url.includes("/_next") ||
+    req.url.includes("/static") ||
+    req.url.includes("favicon.ico") ||
+    req.url.includes(".well-known")
+  ) {
+    return NextResponse.next();
+  }
+
+  const start = Date.now();
+  const res = NextResponse.next();
+
+  event.waitUntil(
+    (async () => {
+      const duration = Date.now() - start;
+      await fetch(
+        `${
+          process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+        }/api/logger`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            method: req.method,
+            url: req.url,
+            ip,
+            ts: new Date().toISOString(),
+            status: res.status,
+            duration,
+          }),
+        }
+      ).catch(() => {});
+    })()
+  );
+
+  return res;
+}
