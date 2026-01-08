51b3d831bbba065e8ff282e33ceeb931a153a149
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2026-01-08 21:05:13 +0700

    up: α 0.0.51 -> enhance dashboard users getQueryFn to support baseUrlOverride and withCredentials options

--- src/hooks/api/dashboard/use-users.ts
+++ src/hooks/api/dashboard/use-users.ts
@@ -2,20 +2,50 @@ import { BASE_API_URL } from "@/config";
 import { getQueryFn, queryClient } from "@/lib/query-client";
 import { ApiResponse } from "@/types/api-type";
 import { User } from "@/types/dashboard/users-type";
+import { APIResponsePagination } from "@/types/tanstack-table";
 import { axiosReq } from "@/utils/api";
 import { useMutation, useQuery, UseQueryResult } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
-export function useUsers(type?: string): UseQueryResult<User[], Error> {
+// export function useUsers(type?: string): UseQueryResult<User[], Error> {
+//   return useQuery<User[], Error>({
+//     queryKey: type ? ["users", type] : ["users"],
+//     queryFn: getQueryFn<User[]>(),
+//     // placeholderData: {
+//     //   data: [],
+//     //   links: {},
+//     //   meta: {},
+//     // }
+//     // enabled: !!type, // kalau mau fetch cuma kalau type ada
+//   });
+// }
+
+export function useUsers(
+  page = 1,
+  limit = 5,
+  search = "",
+  sortBy = "id",
+  sortOrder = "desc"
+) {
   return useQuery<User[], Error>({
-    queryKey: type ? ["users", type] : ["users"],
+    queryKey: ["/users", { page, limit, search, sortBy, sortOrder }],
     queryFn: getQueryFn<User[]>(),
-    // placeholderData: {
-    //   data: [],
-    //   links: {},
-    //   meta: {},
-    // }
-    // enabled: !!type, // kalau mau fetch cuma kalau type ada
+  });
+}
+
+export function useUsersCustomApi(
+  baseUrl: string,
+  page = 1,
+  limit = 10,
+  search = "",
+  sortBy = "role",
+  sortOrder = "asc"
+) {
+  return useQuery<APIResponsePagination<User>, Error>({
+    queryKey: ["/users", { page, limit, search, sortBy, sortOrder }],
+    queryFn: getQueryFn<APIResponsePagination<User>>({
+      baseUrlOverride: baseUrl,
+    }),
   });
 }
 
